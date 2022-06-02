caf3ff8e681769353db4abd8db70bef6b83c1abb
kowalskiCES <albertoramosmorlanes@gmail.com>
2022-06-02 17:25:08 +0200

    COMMIT INICIAL

---
+++ src/agenda/Agenda.java
@@ -0,0 +1,59 @@
+
+package agenda;
+
+//import exceptions.ContactoInexistenteException;
+import exceptions.ContactoRepetidoException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ *
+ * @author loned
+ */
+public class Agenda {
+
+    private HashMap<String, Contacto> contactos = new HashMap();
+    private static int n = 0;
+    private static int count = 0;
+ 
+    public Contacto addContacto(Contacto contacto) throws ContactoRepetidoException {
+        if (contactos.containsKey(contacto.getEmail())) {
+            //
+            throw new ContactoRepetidoException("Ya existe un contacto con email: " + contacto.getEmail());
+        }
+        n++;
+        contacto.setN(n);
+        contactos.put(contacto.getEmail(), contacto);
+        count++;
+        return contacto;
+    }
+
+    public Contacto eliminarContacto(String email) { 
+        Contacto contacto = buscarContacto(email);
+        if (contacto != null) {
+        contactos.remove(email);
+        count--;
+        }
+        return contacto;
+    }
+
+    public Contacto buscarContacto(String email) { 
+        Contacto contacto = null;
+        if (!contactos.containsKey(email)) {
+
+            return null;
+        }
+        else{
+        return contactos.get(email);
+        }
+    }
+
+    public ArrayList<Contacto> getContactos() {
+        ArrayList<Contacto> contactosList = new ArrayList<>();
+        contactosList.addAll(contactos.values());
+        return contactosList;
+    }
+    
+    
+
+}
